7ff3316e5e87df14f38a6773eae99e6cdf0894b0
Thomas Karner <thomas.7.karner@gmail.com>
2023-04-23 19:00:45 +0200

    remove searc bar from now

--- web/src/routes/lookout/+page.svelte
+++ web/src/routes/lookout/+page.svelte
@@ -29,24 +29,7 @@
   <PageHeader title="Lookouts" />
 
   <div class="flex items-center pb-3">
-    <div class="relative">
-      <div
-        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
-      >
-        <span class="pr-2">
-          {@html iconData.search}
-        </span>
-      </div>
-      <input
-        type="text"
-        id="table-search"
-        class="block p-2 pl-10 text-sm text-gray-900 focus:outline-none border border-gray-300 rounded-lg w-80 bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Search for items"
-      />
-    </div>
-
     <span class="grow" />
-
     <Button
       title="Create"
       leadingIcon="plus"
