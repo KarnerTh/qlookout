6a9b53528dae1538d7f0fd8e897109d2b422096c
Thomas Karner <thomas.7.karner@gmail.com>
2023-04-25 19:58:59 +0200

    add logo

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import "../app.css";
-  import NavigationHeader from "../lib/components/navigation/NavigationHeader.svelte";
   import NavigationItem from "../lib/components/navigation/NavigationItem.svelte";
   import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client/core";
   import { setClient } from "svelte-apollo";
+  import logo from "$lib/assets/logo.svg";
 
   const client = new ApolloClient({
     link: new HttpLink({ uri: "http://localhost:8080/query" }),
@@ -17,23 +17,24 @@
   class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800"
 >
   <div class="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
-    <div class="flex items-center justify-center h-14 border-b">
-      <div class="font-bold">Query-Lookout</div>
+    <div class="flex flex-col items-center justify-center border-b">
+      <img alt="logo" class="h-24 my-2" src={logo} />
+      <div class="font-bold mb-2">Query-Lookout</div>
     </div>
     <div class="overflow-y-auto overflow-x-hidden flex-grow">
       <ul class="flex flex-col py-4 space-y-1">
-        <NavigationHeader title="Menu" />
-        <NavigationItem icon="dashboard" title="Dashboard" link="/dashboard" />
+        <!-- <NavigationHeader title="Menu" /> -->
+        <!-- <NavigationItem icon="dashboard" title="Dashboard" link="/dashboard" /> -->
         <NavigationItem icon="lookouts" title="Lookouts" link="/lookout" />
-        <NavigationItem
-          icon="notification"
-          title="Notifications"
-          link="/notifications"
-          badge="12"
-        />
-        <NavigationHeader title="Settings" />
-        <NavigationItem icon="settings" title="Settings" link="/settings" />
-        <NavigationItem icon="logout" title="Logout" link="/logout" />
+        <!-- <NavigationItem -->
+        <!--   icon="notification" -->
+        <!--   title="Notifications" -->
+        <!--   link="/notifications" -->
+        <!--   badge="12" -->
+        <!-- /> -->
+        <!-- <NavigationHeader title="Settings" /> -->
+        <!-- <NavigationItem icon="settings" title="Settings" link="/settings" /> -->
+        <!-- <NavigationItem icon="logout" title="Logout" link="/logout" /> -->
       </ul>
     </div>
   </div>
