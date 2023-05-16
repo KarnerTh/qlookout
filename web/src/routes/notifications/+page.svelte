f2fa0f4ba6659b5bde471cbf472df8945a3fe123
Thomas Karner <thomas.7.karner@gmail.com>
2023-05-16 19:31:54 +0200

    make list blazingly fast

--- web/src/routes/notifications/+page.svelte
+++ web/src/routes/notifications/+page.svelte
@@ -3,9 +3,10 @@
   import PageHeader from "$lib/components/header/PageHeader.svelte";
   import NotificationListItem from "$lib/components/notification/NotificationListItem.svelte";
   import { notifications } from "$lib/stores/notification.store";
+  import VirtualList from "@sveltejs/svelte-virtual-list";
 </script>
 
-<div class="flex flex-col max-h-full">
+<div class="flex flex-col max-h-full h-full">
   <PageHeader title="Notifications" />
   <div class="flex items-center pb-3">
     <span class="grow" />
@@ -18,15 +19,15 @@
     />
   </div>
 
-  <div class="overflow-auto mb-2">
+  <div class="overflow-auto mb-2 h-full">
     {#if $notifications.length === 0}
       <div class="flex items-center justify-center pt-5">No notifications</div>
     {:else}
-      {#each $notifications as notification (notification)}
-        <div class="mb-2">
-          <NotificationListItem {notification} />
-        </div>
-      {/each}
+        <VirtualList items={$notifications} let:item>
+          <div class="mb-2">
+            <NotificationListItem notification={item} />
+          </div>
+        </VirtualList>
     {/if}
   </div>
 </div>
